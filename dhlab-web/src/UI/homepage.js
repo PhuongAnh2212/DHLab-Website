f9b17d1f5b3782a7282f7100357e245173c30eea
Phuong Anh <119726597+PhuongAnh2212@users.noreply.github.com>
2024-03-26 23:34:38 +0700

    Test

--- dhlab-web/src/UI/homepage.js
+++ dhlab-web/src/UI/homepage.js
@@ -4,6 +4,7 @@ function HomePage() {
   return (
     <div>
       <h2>This is the Home Page</h2>
+      <h3>Test DHLab Website</h3>
       {/* Add your UI components and content here */}
     </div>
   );
